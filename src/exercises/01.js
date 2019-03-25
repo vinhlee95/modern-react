d688dba28cd7fb18cb1f3c64665ad55971b5a8c0
Vinh Le <lethanhvinh95@gmail.com>
2019-03-25 21:16:42 +0200

    exercise 1
    Finish exercise 1

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -1,15 +1,9 @@
-// Counter: useState
-// 🐨 you'll need to add {useState} to this import statement
-import React from 'react'
-
-// 💰 the `useState` hook allows you to use state
-// from within function components in react:
-// const [name, setName] = useState('Angela')
+import React, {useState} from 'react'
 
 function Counter() {
-  // 🐨 you'll call useState here to get count and setCount
-  // 🐨 render the count here and add an onClick handler that increments the count
-  return <button>TODO</button>
+  const [counter, setCounter] = useState(0)
+
+  return <button onClick={() => setCounter(counter + 1)}>{counter}</button>
 }
 
 // Don't make changes to the Usage component. It's here to show you how your
